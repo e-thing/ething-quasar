d90d655282874f3d565f6573743a425d3620c2c8
Adrien Mezerette <a.mezerette@gmail.com>
2019-01-21 19:18:29 +0100

    add json formatter

---
+++ src/components/JsonFormatter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div ref="content">
+</template>
+
+<script>
+import JSONFormatter from 'json-formatter-js'
+
+export default {
+    name: 'JsonFormatter',
+
+    props: ['value'],
+
+    data () {
+        return {
+
+        }
+    },
+
+    watch: {
+      value: {
+        handler (val) {
+          const formatter = new JSONFormatter(val);
+          var el = this.$refs.content
+          while (el.firstChild) {
+              el.removeChild(el.firstChild);
+          }
+          el.appendChild(formatter.render());
+        },
+        immediate: true
+      }
+
+    },
+
+}
+</script>
